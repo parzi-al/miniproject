60639b75ad01a8e7f2ce6d9dd7cb0d9aee6fc450
Bhavan S <109290304+parzi-al@users.noreply.github.com>
2024-03-24 14:33:50 +0530

    updated 2

---
+++ lib/screen/register.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class Registers extends StatelessWidget {
+  const Registers({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("data") ;
+  }
+}
