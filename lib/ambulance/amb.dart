9286867c5b4e61d8a77b5c473b39f35103349f3c
aasher-57020 <aasherpaulpadayatty@gmail.com>
2024-04-09 23:00:06 +0530

    ambulance driver update

--- lib/ambulance/amb.dart
+++ lib/ambulance/amb.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hexcolor/hexcolor.dart';
 import 'package:flutter_application_1/components/alert.dart';
+import 'package:flutter_application_1/ambulance/ambsvr.dart';
 class Amb extends StatefulWidget {
   const Amb({Key? key}) : super(key: key);
-  
+
   @override
   State<Amb> createState() => _AmbState();
 }
@@ -14,63 +15,132 @@ class _AmbState extends State<Amb> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: HexColor('#2B2D42'),
-        appBar: AppBar(
-          title: Center(
-            child: Text(
-              'HOSPIFY',
-              style: GoogleFonts.inter(
-                textStyle: TextStyle(
-                  fontSize: 43.92,
-                  fontWeight: FontWeight.w800,
-                  color: HexColor('#8D99AE'),
-                ),
+      appBar: AppBar(
+        title: Center(
+          child: Text(
+            'HOSPIFY',
+            style: GoogleFonts.inter(
+              textStyle: TextStyle(
+                fontSize: 43.92,
+                fontWeight: FontWeight.w800,
+                color: HexColor('#8D99AE'),
               ),
             ),
           ),
-          backgroundColor: HexColor('#2B2D42'),
         ),
-        body: Center(
-          child: GestureDetector(
-            onTap: () {
-              Navigator.push(
-                  context,
-                  PageRouteBuilder(
-                    pageBuilder: (context, animation1, animation2) =>
-                        AlertScreen(),
-                    transitionDuration: Duration(seconds: 1),
-                    transitionsBuilder:
-                        (context, animation, secondaryAnimation, child) {
-                      return FadeTransition(opacity: animation, child: child);
-                    },
-                  ));
-            },
-            child: Container(
-              width: 313.0, // diameter 50
-              height: 313.0, // diameter 50
-              decoration: BoxDecoration(
-                color: HexColor("#EDF6FF"),
-                shape: BoxShape.circle,
-                border: Border.all(
-                  color: HexColor("#8D99AE"),
-                  width: 5.0,
-                ),
+        backgroundColor: HexColor('#2B2D42'),
+      ),
+      body: Center(
+        child: GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              PageRouteBuilder(
+                pageBuilder: (context, animation1, animation2) => AmbNew(),
+                transitionDuration: Duration(seconds: 1),
+                transitionsBuilder:
+                    (context, animation, secondaryAnimation, child) {
+                  return FadeTransition(opacity: animation, child: child);
+                },
               ),
-              child: Center(
-                child: Text(
-                  "No New Alerts",
-                  style: TextStyle(
-                    color: HexColor("#2B2D42"),
-                    fontSize: 40.0,
-                    fontFamily: 'Inter',
-                    fontWeight: FontWeight.w900,
-                    height: 0,
-                  ),
+            );
+          },
+          child: Container(
+            width: 313.0, // diameter 50
+            height: 313.0, // diameter 50
+            decoration: BoxDecoration(
+              color: HexColor("#EDF6FF"),
+              shape: BoxShape.circle,
+              border: Border.all(
+                color: HexColor("#8D99AE"),
+                width: 5.0,
+              ),
+            ),
+            child: Center(
+              child: Text(
+                "No New Alerts",
+                style: TextStyle(
+                  color: HexColor("#2B2D42"),
+                  fontSize: 40.0,
+                  fontFamily: 'Inter',
+                  fontWeight: FontWeight.w900,
                 ),
               ),
             ),
           ),
-        )
-        );
+        ),
+      ),
+    );
   }
 }
 
+class AmbNew extends StatefulWidget {
+  const AmbNew({Key? key}) : super(key: key);
+
+  @override
+  State<AmbNew> createState() => _AmbNewState();
+}
+
+class _AmbNewState extends State<AmbNew> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: HexColor('#2B2D42'),
+      appBar: AppBar(
+        title: Center(
+          child: Text(
+            'HOSPIFY',
+            style: GoogleFonts.inter(
+              textStyle: TextStyle(
+                fontSize: 43.92,
+                fontWeight: FontWeight.w800,
+                color: HexColor("#EF233C"),
+              ),
+            ),
+          ),
+        ),
+        backgroundColor: HexColor('#2B2D42'),
+      ),
+      body: Center(
+        child: GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              PageRouteBuilder(
+                pageBuilder: (context, animation1, animation2) => Ambsvr(),
+                transitionDuration: Duration(seconds: 1),
+                transitionsBuilder:
+                    (context, animation, secondaryAnimation, child) {
+                  return FadeTransition(opacity: animation, child: child);
+                },
+              ),
+            );
+          },
+          child: Container(
+            width: 313.0, // diameter 50
+            height: 313.0, // diameter 50
+            decoration: BoxDecoration(
+              color: HexColor("#EF233C"),
+              shape: BoxShape.circle,
+              border: Border.all(
+                color: HexColor("#8D99AE"),
+                width: 5.0,
+              ),
+            ),
+            child: Center(
+              child: Text(
+                "NEW ALERT!",
+                style: TextStyle(
+                  color: HexColor("#FFFFFF"),
+                  fontSize: 40.0,
+                  fontFamily: 'Inter',
+                  fontWeight: FontWeight.w900,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/ambulance/ambsvr.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Ambsvr extends StatefulWidget {
+  const Ambsvr({super.key});
+
+  @override
+  State<Ambsvr> createState() => _AmbsvrState();
+}
+
+class _AmbsvrState extends State<Ambsvr> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
